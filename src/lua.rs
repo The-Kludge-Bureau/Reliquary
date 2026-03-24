c5af63518f9eae4acb26681e051c001569e2e189
txtsd <code@ihavea.quest>
2026-03-24 10:20:01 +0530

    feat: add offsets and lua wrappers from HearthDB
    Signed-off-by: txtsd <code@ihavea.quest>

--- src/lua.rs
+++ src/lua.rs
@@ -1,1 +1,104 @@
-// TODO
+//! Thin unsafe wrappers for the Lua 5.0 API functions embedded in the
+//! WoW 1.12.1 client. All functions are called through raw function
+//! pointers resolved from the offsets in offsets.rs.
+//!
+//! The Lua state is represented as *mut usize to match the client's
+//! non-standard embedding, consistent with how nampower handles it.
+
+use crate::offsets;
+use std::ffi::{CStr, CString};
+
+pub type LuaState = *mut usize;
+
+/// Returns the active Lua state from the game process.
+pub unsafe fn get_lua_state() -> LuaState {
+    let f: unsafe extern "fastcall" fn() -> LuaState =
+        std::mem::transmute(offsets::LUA_STATE_PTR);
+    f()
+}
+
+pub unsafe fn lua_error(l: LuaState, msg: &str) {
+    let s = CString::new(msg).unwrap_or_default();
+    let f: unsafe extern "cdecl" fn(LuaState, *const i8) =
+        std::mem::transmute(offsets::LUA_ERROR);
+    f(l, s.as_ptr());
+}
+
+pub unsafe fn lua_gettop(l: LuaState) -> i32 {
+    let f: unsafe extern "fastcall" fn(LuaState) -> i32 =
+        std::mem::transmute(offsets::LUA_GETTOP);
+    f(l)
+}
+
+pub unsafe fn lua_isstring(l: LuaState, idx: i32) -> bool {
+    let f: unsafe extern "fastcall" fn(LuaState, i32) -> bool =
+        std::mem::transmute(offsets::LUA_ISSTRING);
+    f(l, idx)
+}
+
+pub unsafe fn lua_isnumber(l: LuaState, idx: i32) -> bool {
+    let f: unsafe extern "fastcall" fn(LuaState, i32) -> bool =
+        std::mem::transmute(offsets::LUA_ISNUMBER);
+    f(l, idx)
+}
+
+pub unsafe fn lua_tostring(l: LuaState, idx: i32) -> Option<String> {
+    let f: unsafe extern "fastcall" fn(LuaState, i32) -> *const i8 =
+        std::mem::transmute(offsets::LUA_TOSTRING);
+    let ptr = f(l, idx);
+    if ptr.is_null() {
+        None
+    } else {
+        Some(CStr::from_ptr(ptr).to_string_lossy().into_owned())
+    }
+}
+
+pub unsafe fn lua_tonumber(l: LuaState, idx: i32) -> f64 {
+    let f: unsafe extern "fastcall" fn(LuaState, i32) -> f64 =
+        std::mem::transmute(offsets::LUA_TONUMBER);
+    f(l, idx)
+}
+
+pub unsafe fn lua_pushnumber(l: LuaState, n: f64) {
+    let f: unsafe extern "fastcall" fn(LuaState, f64) =
+        std::mem::transmute(offsets::LUA_PUSHNUMBER);
+    f(l, n);
+}
+
+pub unsafe fn lua_pushstring(l: LuaState, s: &str) {
+    let cs = CString::new(s).unwrap_or_default();
+    let f: unsafe extern "fastcall" fn(LuaState, *const i8) =
+        std::mem::transmute(offsets::LUA_PUSHSTRING);
+    f(l, cs.as_ptr());
+}
+
+pub unsafe fn lua_pushnil(l: LuaState) {
+    let f: unsafe extern "fastcall" fn(LuaState) =
+        std::mem::transmute(offsets::LUA_PUSHNIL);
+    f(l);
+}
+
+pub unsafe fn lua_newtable(l: LuaState) {
+    let f: unsafe extern "fastcall" fn(LuaState) =
+        std::mem::transmute(offsets::LUA_NEWTABLE);
+    f(l);
+}
+
+pub unsafe fn lua_settable(l: LuaState, idx: i32) {
+    let f: unsafe extern "fastcall" fn(LuaState, i32) =
+        std::mem::transmute(offsets::LUA_SETTABLE);
+    f(l, idx);
+}
+
+pub unsafe fn lua_rawseti(l: LuaState, t: i32, n: i32) {
+    let f: unsafe extern "fastcall" fn(LuaState, i32, i32) =
+        std::mem::transmute(offsets::LUA_RAWSETI);
+    f(l, t, n);
+}
+
+pub unsafe fn register_lua_function(name: &str, func: *mut usize) {
+    let cs = CString::new(name).unwrap_or_default();
+    let f: unsafe extern "fastcall" fn(*const i8, *mut usize) =
+        std::mem::transmute(offsets::FRAME_SCRIPT_REGISTER_FUNCTION);
+    f(cs.as_ptr(), func);
+}
--- src/offsets.rs
+++ src/offsets.rs
@@ -1,1 +1,24 @@
-// TODO
+//! Hard-coded offsets for the TurtleWoW 1.12.1 (build 5875) client.
+//! All values are absolute virtual addresses in the 32-bit process space.
+
+pub const PLAYER_LOAD_SCRIPT_FUNCTIONS: usize   = 0x0049_0250;
+pub const GLUE_LOAD_SCRIPT_FUNCTIONS: usize     = 0x0046_ABB0;
+pub const FRAME_SCRIPT_REGISTER_FUNCTION: usize = 0x0070_4120;
+
+/// Returns the current lua_State* via __fastcall with no arguments.
+pub const LUA_STATE_PTR: usize = 0x0070_40D0;
+
+pub const LUA_ERROR:       usize = 0x006F_4940;
+pub const LUA_GETTOP:      usize = 0x006F_3070;
+pub const LUA_SETTOP:      usize = 0x006F_3080;
+pub const LUA_ISSTRING:    usize = 0x006F_3510;
+pub const LUA_ISNUMBER:    usize = 0x006F_34D0;
+pub const LUA_TOSTRING:    usize = 0x006F_3690;
+pub const LUA_TONUMBER:    usize = 0x006F_3620;
+pub const LUA_PUSHNUMBER:  usize = 0x006F_3810;
+pub const LUA_PUSHSTRING:  usize = 0x006F_3890;
+pub const LUA_PUSHNIL:     usize = 0x006F_37F0;
+pub const LUA_PUSHBOOLEAN: usize = 0x006F_39F0;
+pub const LUA_NEWTABLE:    usize = 0x006F_3C90;
+pub const LUA_SETTABLE:    usize = 0x006F_3E20;
+pub const LUA_RAWSETI:     usize = 0x006F_3F60;
